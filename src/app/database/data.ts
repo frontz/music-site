48e2ce3c489f8cb06f2b987b5dd4ae300cc2cf38
Tomasz Fraczek <tom.frontz@gmail.com>
2023-09-08 12:00:39 +0200

    Fix errors in ids of data

--- src/app/database/data.ts
+++ src/app/database/data.ts
@@ -3,7 +3,7 @@ import {Album} from "../models/album.model";
 export class Data {
   albums: Album[] = [
     {id: 1, title: "Piano Solo", price: 9.50},
-    {id: 1, title: "Deep Blue", price: 10.50},
-    {id: 1, title: "Words & Tears", price: 1.90}
+    {id: 2, title: "Deep Blue", price: 10.50},
+    {id: 3, title: "Words & Tears", price: 1.90}
   ];
 }
