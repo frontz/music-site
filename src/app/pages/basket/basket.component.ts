3260c5d0702107721ed4c532511f248d16a828e2
Tomasz Fraczek <tom.frontz@gmail.com>
2023-10-06 09:55:22 +0200

    Project cleaned from previous versions of Stripe

--- src/app/pages/basket/basket.component.ts
+++ src/app/pages/basket/basket.component.ts
@@ -19,7 +19,6 @@ export class BasketComponent implements OnInit {
   ngOnInit() {
     this.items = this.basket.getItems();
     this.basket.getItems();
-    this.invokeStripe();
 
   }
 
@@ -32,42 +31,5 @@ export class BasketComponent implements OnInit {
   getTotalPrice() {
     return this.basket.totalPrice();
   }
-
-  makePayment(amount: any) {
-    const paymentHandler = (<any>window).StripeCheckout.configure({
-      key: 'pk_test_51LYlpzDy80XW6LItexqrVhaikfEyEBlsEOsTiUc8f71ZRXVQ6yJfUTOCjZZQbKUrrCdFBwtfTGoZS86OsWmNZZIT00kVB1uSwL',
-      locale: 'auto',
-      token: function (stripeToken: any) {
-        console.log(stripeToken);
-        alert('Stripe token generated!');
-      },
-    });
-    paymentHandler.open({
-      name: 'Your credit cart data',
-      description: 'Payment for your order',
-      amount: amount * 100,
-    });
-  }
-  invokeStripe() {
-    if (!window.document.getElementById('stripe-script')) {
-      const script = window.document.createElement('script');
-      script.id = 'stripe-script';
-      script.type = 'text/javascript';
-      script.src = 'https://checkout.stripe.com/checkout.js';
-      
-      script.onload = () => {
-        this.paymentHandler = (<any>window).StripeCheckout.configure({
-          key: 'pk_test_51LYlpzDy80XW6LItexqrVhaikfEyEBlsEOsTiUc8f71ZRXVQ6yJfUTOCjZZQbKUrrCdFBwtfTGoZS86OsWmNZZIT00kVB1uSwL',
-          locale: 'auto',
-          token: function (stripeToken: any) {
-            console.log(stripeToken);
-            alert('Payment has been successfull!');
-          },
-        });
-      };
-      window.document.body.appendChild(script);
-    }
-  }
-
 
 }
