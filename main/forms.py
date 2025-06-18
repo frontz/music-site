9b133ac2ec474ad910c262429e6fcc6897d227cf
Tomasz <tom.frontz@gmail.com>
2025-06-18 08:49:52 +0200

    Email form with labels and placeholders

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,19 @@
 from django import forms
 from django.conf import settings
 
+
 class EmailForm(forms.Form):
-    name = forms.CharField(max_length=30)
-    email = forms.EmailField()
+    name = forms.CharField(label='Name', max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Type your name please'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'placeholder': 'Your email'}))
     to = forms.EmailField(
         widget=forms.HiddenInput(),
         initial=settings.DEFAULT_TO_EMAIL
     )
     message = forms.CharField(
+        label='Message',
         required=True,
-        widget=forms.Textarea
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Type your message...'
+        })
     )
